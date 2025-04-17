4f783ecd4e57184719f57463f2a1399438081b45
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2025-04-16 22:37:24 -0400

    Fix user service yml file

--- user-service/app/core/config.py
+++ user-service/app/core/config.py
@@ -1,23 +1,40 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+from dotenv import load_dotenv
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Manually load .env file
+load_dotenv()
 
 class Settings(BaseSettings):
-    mongo_url: str = "mongodb://root:example@mongodb:27017"
-    mongo_db_name: str = "users"
-    mongo_user: str = "root"
-    mongo_pass: str = "example"
+    mongo_user: str = os.getenv("MONGO_USER", "root")
+    mongo_pass: str = os.getenv("MONGO_PASS", "example")
+    mongo_host: str = os.getenv("MONGO_HOST", "mongodb")
+    mongo_port: str = os.getenv("MONGO_PORT", "27017")
+    mongo_db_name: str = os.getenv("MONGO_DB_NAME", "mydb")
 
-    # RabbitMQ credentials
-    rabbitmq_url: str = "amqp://guest:guest@host.docker.internal:5672"
-    rabbitmq_exchange: str = "user_events"
-    rabbitmq_user: str = "guest"
-    rabbitmq_pass: str = "guest"
+    rabbitmq_user: str = os.getenv("RABBITMQ_USER", "guest")
+    rabbitmq_pass: str = os.getenv("RABBITMQ_PASS", "guest")
+    rabbitmq_host: str = os.getenv("RABBITMQ_HOST", "host.docker.internal")
+    rabbitmq_port: str = os.getenv("RABBITMQ_PORT", "5672")
+    rabbitmq_exchange: str = os.getenv("RABBITMQ_EXCHANGE", "user_events")
 
-    secret_key: str = "rqwerwqerwqr"  # Should be a long, random string
+    secret_key: str = os.getenv("SECRET_KEY", "rqwerwqerwqr")
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+    @property
+    def mongo_url(self) -> str:
+        url = f"mongodb://{self.mongo_user}:{self.mongo_pass}@{self.mongo_host}:{self.mongo_port}"
+        logger.info(f"Mongo URL: {url}")
+        return url
+
+    @property
+    def rabbitmq_url(self) -> str:
+        url = f"amqp://{self.rabbitmq_user}:{self.rabbitmq_pass}@{self.rabbitmq_host}:{self.rabbitmq_port}"
+        logger.info(f"RabbitMQ URL: {url}")
+        return url
 
 @lru_cache()
 def get_settings():
